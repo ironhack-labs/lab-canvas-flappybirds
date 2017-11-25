9c8775e425a6dab81d448961fb57c7460c80cb6f
Felix Heras <heras.felix@gmail.com>
2017-11-25 16:43:32 +0100

    feat: some properties

--- starter_code/flappy.js
+++ starter_code/flappy.js
@@ -11,4 +11,19 @@ function Flappy(id) {
     this.ctx.drawImage(img, 0, 0, 50, 50);
   }).bind(this);
 
+  function Fly(){
+  this.x = 50;
+  this.y = 50;
+  this.sx = 2;
+  this.sy = 2;
+}
+
+  this.x += this.sx;
+  this.y += this.sy;
+
+
+  setInterval(function() {
+    Fly.update();
+  }, 20);
+
 }
