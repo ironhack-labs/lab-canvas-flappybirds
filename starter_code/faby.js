ab80148a698405048453751fc0e1d68c10dc3675
pabloonieva <pablo@ironhack.com>
2017-11-26 11:16:54 +0100

    feat: first commit

---
+++ starter_code/faby.js
@@ -0,0 +1,12 @@
+function Faby (canvas, sprite){
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext ("2d");
+  this.width = 20;
+  this.height = 20;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.gravity = 0.5;
+  this.gravitySpeed = 10;
+
+
+}
