02695a644d1bbe6c89e26f297cc746367b180dda
Ana Recuero <anarecsa@gmail.com>
2018-01-19 15:42:46 +0100

    game over when bird exits the board

--- starter_code/js/bird.js
+++ starter_code/js/bird.js
@@ -1,3 +1,4 @@
+var img = new Image();
 function createBird(ctx, canvasSize){
   var bird = {
       x: 40,
@@ -7,31 +8,20 @@ function createBird(ctx, canvasSize){
       userPull: 0,
       radius: 25,
       color: '#2e7d32',
-      render: function () {
-          // NOTE: Optimizar, no renderiza cuando no se ve la bola 
-          //if(this.y > canvasSize.h + this.radius){ return; }
-          //if(this.x > canvasSize.w + this.radius){ return; }
+      imgScale: 500/350,
   
-          ctx.font = "15px Arial";
-          ctx.save()
-          ctx.translate(this.x + 30, this.y + 30);
-          var txt = " vy: " + this.vy.toFixed(2);
-          ctx.fillText(txt,10,50);
-          ctx.restore()
-          var img = new Image();
-          imgScale = 500/350;
+      render: function () {
           img.src="./images/flappy.png";
-        //img.onload = function() {
-            ctx.drawImage(img, this.x, this.y, 50*imgScale,50);
-          //}
+          ctx.drawImage(img, this.x, this.y, 50*this.imgScale,50);
 
       },
       update: function(){
           this.vy += (window.gravity - this.userPull);
           //this.x += this.vx;
           this.y += this.vy;
-          if (this.y + this.vy > canvasSize.h || this.y + this.vy < 0) {
+          if (this.y + this.vy > canvasSize.h - 47 || this.y + this.vy < 0) {
               this.vy *= -1.0;
+              window.alert("GAME OVER!!");
           }
           // if (this.x + this.vx > canvasSize.w || this.x + this.vx < 0) {
           //     this.vx *= -1.0;
--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    document.getElementById("start-button").setAttribute("class","disabled");
     startGame();
   };
 
   function startGame() {
+    
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var canvasSize = { w: canvas.width, h: canvas.height };
