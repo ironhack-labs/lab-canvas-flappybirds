6e0efc9c6e175756c7cd1a81a37d88f9fec7c704
Giorgio Grassini <giorgiograssini@gmail.com>
2018-01-21 17:33:11 +0100

    Add obstacle bottom

---
+++ starter_code/flappy.js
@@ -0,0 +1,12 @@
+function flappy(){
+    this.y = height/2;
+    this.x = 64;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 1;
+    this.gravitySpeed = 0;
+};
+
+flappy.prototype.update() = {
+    this()
+}
--- starter_code/script.js
+++ starter_code/script.js
@@ -2,69 +2,123 @@ window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
+};
+
+  var gravity = 2;
+
+function startGame() {
+  var img = new Image();
+  img.src = "images/bg.png";
+  var imgFlappy = new Image();
+  imgFlappy.src = "images/flappy.png";
+  var imgObst = new Image();
+  imgObst.src = "images/obstacle_bottom.png";
+  var imgObstTop = new Image();
+  imgObstTop.src = 'images/obstacle_top.png';
 
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
-  function startGame() {
-    var img = new Image();
-    img.src = 'images/bg.png';
-    var CanvasXSize = 750;
-    var CanvasYSize = 500;
-    var speed = 30;
-    var scale = 1.05;
-    var y = 0;
+  var backgroundImage = {
+    img: img,
+    x: 0,
+    speed: -3,
 
-    var dx = 0.75;
-    var imgW;
-    var imgH;
-    var x = 0;
-    var clearX;
-    var clearY;
-    var ctx;
+    move: function() {
+      this.x += this.speed;
+      this.x %= canvas.width;
+    },
 
-    img.onload = function() {
-      imgW = img.width * scale;
-      imgH = img.height * scale;
-      if (imgW > CanvasXSize) {
-        x = CanvasXSize - imgW;
-      }
-      if (imgW > CanvasXSize) {
-        clearX = imgW;
+    draw: function() {
+      ctx.drawImage(this.img, this.x, 0);
+      if (this.speed < 0) {
+        ctx.drawImage(this.img, this.x + canvas.width, 0);
       } else {
-        clearX = CanvasXSize;
+        ctx.drawImage(this.img, this.x - this.img.width, 0);
       }
-      if (imgH > CanvasYSize) {
-        clearY = imgH;
-      } else {
-        clearY = CanvasYSize;
-      }
-      ctx = document.getElementById("canvas").getContext("2d");
-      return setInterval(draw, speed);
-    };
-
-    function draw() {
-      ctx.clearRect(0, 0, clearX, clearY);
-      if (imgW <= CanvasXSize) {
-        if (x > CanvasXSize) {
-          x = -imgW + x;
-        }
-        if (x > 0) {
-          ctx.drawImage(img, -imgW + x, y, imgW, imgH);
-        }
-        if (x - imgW > 0) {
-          ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
-        }
-      } else {
-        if (x > CanvasXSize) {
-          x = CanvasXSize - imgW;
-        }
-        if (x > CanvasXSize - imgW) {
-          ctx.drawImage(img, x - imgW + 1, y, imgW, imgH);
-        }
-      }
-      ctx.drawImage(img, x, y, imgW, imgH);
-      x += dx;
     }
+  };
+  function updateCanvas() {
+    backgroundImage.move();
+    obst.move();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    backgroundImage.draw();
+
+    drawFlappy(flappy);
+    flappy.y+=gravity;
+    obst.draw();
+    //obst.drawTop();
+    if(flappy.y>450 || (flappy.x < obst.x && flappy.y>obst.y)){
+      alert("GAME OVER");
+      document.location.reload();
+    }
+    requestAnimationFrame(updateCanvas);
   }
-};
+  img.onload = updateCanvas;
+
+  var flappy = {
+    x: 25,
+    y: canvas.width/3,
+    moveUp: function() {
+      this.y -= 30;
+    },
+  };
+
+  function drawFlappy(flappy) {
+    // imgFlappy.onload = function() {
+    ctx.drawImage(imgFlappy, flappy.x, flappy.y, 50, 50);
+    // };
+  }
+
+  document.onkeydown = function(e) {
+    e.preventDefault();
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 32:
+        flappy.moveUp();
+        console.log("up", flappy);
+        break;
+    }
+    updateFlappy();
+  };
+
+  function updateFlappy() {
+    ctx.clearRect(0, 0, 1500, 1700);
+    ctx.fillText("Ghost_x: " + flappy.x, 580, 40);
+    ctx.fillText("Ghost_y: " + flappy.y, 580, 60);
+    drawFlappy(flappy);
+  }
+
+  updateFlappy();
+
+  var obst = {
+    img : imgObst,
+    imgTop: imgObstTop,
+    x : 0,
+    y:400,
+    speed : -3,
+    
+    move: function() {
+      this.x += this.speed;
+      this.x %= canvas.width;
+    },
+    draw: function() {
+      ctx.drawImage(this.img, this.x, this.y);
+      if (this.speed < 0) {
+        ctx.drawImage(this.img, this.x + canvas.width, this.y);
+      } else {
+        ctx.drawImage(this.img, this.x - this.img.width, this.y);
+      }
+    },
+    drawTop: function() {
+      ctx.drawImage(this.imgObstTop, this.x, this.y);
+      if (this.speed < 0) {
+        ctx.drawImage(this.imgObstTop, this.x + canvas.width, this.y);
+      } else {
+        ctx.drawImage(this.imgObstTop, this.x - this.img.width, this.y);
+      }
+  }
+
+}
+}
