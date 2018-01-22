c939543eea7fd622732fa4ffb8cf675f87421250
Carlos San Martin <carlosanmartin@gmail.com>
2018-01-22 16:13:44 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,8 +3,41 @@ window.onload = function() {
     startGame();
   };
 
+  var canvas = document.getElementById("flappy-canvas");
+  var ctx = canvas.getContext("2d");
+
+  var img = new Image();
+  img.src = "images/bg.png";
+  var CanvasXSize = 900;
+  var CanvasYSize = 650;
+  var speed = 30;
+  var scale = 1.05;
+  var y = 0;
+
+  var dx = 0.75;
+  var imgW;
+  var imgH;
+  var x = 0;
+  var clearX;
+  var clearY;
+  var ctx;
+
+  img.onload = function() {
+    imgW = img.width * scale;
+    imgH = img.height * scale;
+    if (imgW > CanvasXSize) { 
+      x = CanvasXSize - imgW; }
+    if (imgW > CanvasXSize) { clearX = imgW; }
+    else { clearX = CanvasXSize; }
+    if (imgH > CanvasYSize) { clearY = imgH; }
+    else { clearY = CanvasYSize; }
+    ctx = document.getElementById('flappy-canvas').getContext('2d');
+    return setInterval(draw, speed);
+}
+
   function startGame() {
-
+    
   }
-
 };
+
+
