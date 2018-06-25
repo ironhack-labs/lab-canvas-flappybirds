874d05b41c61a876002412d02c617c18138a6871
Pepe Lora <jose.maria.lora.alarcon@gmail.com>
2018-06-25 10:47:38 +0200

    Flappy Birds

---
+++ background.js
@@ -0,0 +1,22 @@
+function Background() {
+    this.game = game;
+    this.img = new Image();
+    this.img.src = 'images/bg.png';
+    this.x = 0;
+    this.y = 0;
+  
+  }
+  Background.prototype.draw = function () {
+    this.game.ctx.drawImage(this.img,
+      this.x, this.y,
+      this.game.canvas.width,
+      this.game.canvas.height);
+    this.game.ctx.drawImage(this.img,
+      this.x + this.game.canvas.width,
+      this.y,
+      this.game.canvas.width,
+      this.game.canvas.height);
+
+  };
+ 
+  
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,57 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
+
+window.onload = function () {
+  document.getElementById("start-button").onclick = function () {
+    var game = new Game("canvas");
+    game.start();
+    $("#start-button").css({ "pointer-events": "none" }).blur();
   };
-
-  function startGame() {
-
-  }
-
 };
+function Game(canvasID) {
+  this.canvas = document.getElementById(canvasID);
+  this.ctx = canvas.getContext("2d");
+  this.framesCounter = 0
+  this.fps = 60;
+  this.reset();
+
+}
+
+Game.prototype.reset = function(){
+  this.background = new Background(this);
+};  
+
+Game.prototype.draw = function() {
+  this.background.draw()
+}
+
+
+Game.prototype.clear = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+
+Game.prototype.start = function(){
+    this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+    this.background.draw();
+    window.requestAnimationFrame(update);
+  }.bind(this);
+window.requestAnimationFrame(update);
+
+function startGame(){
+  game.start()
+}
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
