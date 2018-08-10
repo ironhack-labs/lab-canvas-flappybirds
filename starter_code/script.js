d764c3f969fd86b492520cf2fc988acc87932ce7
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-10 15:42:09 +0200

    1st iteration: scrolling background

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,18 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let background = new Background();
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    background.drawBackground();
     startGame();
   };
 
   function startGame() {
-
+    setInterval(update, 1000 / 50);
   }
-
 };
+
+function update() {
+  background.drawBackground();
+}
