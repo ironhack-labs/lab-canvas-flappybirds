b73aa647685f5c5ecfba46b84c8ebc7df636198d
Teresa Romero <tromerolozano@gmail.com>
2018-10-06 20:38:20 +0200

    first iteration

---
+++ starter_code/background.js
@@ -0,0 +1,38 @@
+
+var img = new Image();
+img.src = './images/bg.png';
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var backgroundImage = {
+  img: img,
+  x: 0,
+  speed: -0.75,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= canvas.width;
+  },
+
+  draw: function() {
+    ctx.drawImage(this.img, this.x, 0);
+    if (this.speed < 0) {
+      ctx.drawImage(this.img, this.x + canvas.width, 0);
+    } else {
+      ctx.drawImage(this.img, this.x - this.img.width, 0);
+    }
+  },
+};
+
+function updateCanvas() {
+  backgroundImage.move();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImage.draw();
+
+  requestAnimationFrame(updateCanvas);
+}
+
+// start calling updateCanvas once the image is loaded
+img.onload = updateCanvas;
