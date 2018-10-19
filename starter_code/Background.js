547ecb75a4321464e770abcfd2d23ee3dd90e64a
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-10-19 17:19:52 +0200

    Crreated a moving infinite background.

---
+++ starter_code/Background.js
@@ -0,0 +1,22 @@
+class Background {
+    constructor(ctx, url, speed) {
+      this.ctx = ctx
+      this.speed = speed
+      this.img = new Image()
+      this.img.src = url
+      this.x = 0
+      this.height = this.ctx.canvas.height
+      this.width = this.height*this.img.width/this.img.height
+    }
+    update() {
+      this.x -= this.speed
+      if (this.x < -this.width) {
+        this.x += this.width
+      }
+    }
+    draw() {
+        for (var i = 0; this.x+i*this.width < this.ctx.canvas.width; i++) {
+            this.ctx.drawImage(this.img,this.x+i*this.width,0,this.width,this.height)
+        }
+    }
+  }
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,34 @@
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+var bg = new Background(ctx, 'images/bg.png', 1)
+let on = false;
+let intervalId;
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-
+    if(!on){
+      intervalId = setInterval(function(){
+        update();
+        drawEverything();
+      }, 1000/60);
+      on = true;
+    } else {
+      on = false;
+      clearInterval(intervalId);
+    }
   }
-
+  
 };
+
+function update() {
+    bg.update();
+}
+
+function drawEverything() {
+    bg.draw();
+}
