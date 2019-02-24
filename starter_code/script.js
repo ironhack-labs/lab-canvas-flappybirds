e912732d9d54d9b9d9b6753c30d6bbaf9402077c
Josue Valencia <josue.valencia@domo.ec>
2019-02-24 13:00:14 +0100

    mvp obstacles animation

--- starter_code/script.js
+++ starter_code/script.js
@@ -15,6 +15,7 @@ let canvas = {
   canvas : document.createElement('canvas'), 
   speed: -1,
   x : 0,
+  frames: 220,
   start : function () {
     this.context = this.canvas.getContext('2d')
     this.canvas.width = 880;
@@ -41,9 +42,26 @@ let canvas = {
     this.bkgDraw();
   }
 }
+let myObstacles = [];
 
 function updateGame (){
     canvas.updateCanvas()
+    canvas.frames +=1;
+    if (canvas.frames % 240 === 0) {
+      x = canvas.canvas.width;
+      minHeight = 20;
+      maxHeight = 200;
+      height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+      minGap = 50;
+      maxGap = 200;
+      gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+      myObstacles.push(new Component(10, height, "green", x, 0));
+      myObstacles.push(new Component(10, x - height - gap, "green", x, height + gap));
+    }
+    for (i = 0; i < myObstacles.length; i++) {
+      myObstacles[i].x += -1;
+      myObstacles[i].update();
+    }
     requestAnimationFrame(updateGame);
 }
 
@@ -56,7 +74,7 @@ function Component(width, height, color, x, y) {
   this.speedX = 0;
   this.speedY = 0;
   this.update = function(){
-      ctx = myGameArea.context;
+      ctx = canvas.context;
       ctx.fillStyle = color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
   }
