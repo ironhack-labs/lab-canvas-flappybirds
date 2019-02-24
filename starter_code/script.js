1304b085bbed7a8c350f101b3ca5de2f8a49c810
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-02-25 00:39:07 +0100

    probando constructores MAL

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,50 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
+  document.getElementById('start-button').onclick = function() {
     startGame();
   };
 
-  function startGame() {
-
+  // area / lienzo / canvas
+  function MyGameArea() {
+    this.canvas = document.createElement('canvas');
+    
   }
+  
+  MyGameArea.prototype.background = function () {
+    this.canvas.width = 900;
+    this.canvas.height = 400;
+    this.canvas.id = 'canvas';
+    this.context = this.canvas.getContext("2d");
+    document.getElementById('game-board').appendChild(this.canvas);
+    this.img = new Image(600,800);
+    this.img.src = 'images/bg.png';
+    this.img.onload = updateGame();
+    
+  
+  }
+  function updateGame() {
+    console.log(this.img.src)
+    
+  }
+  var myGameArea = new MyGameArea();
+  myGameArea.background();
+  
+  
+  
+  
+  
+
 
 };
+
+
+//var myGameArea = {
+//  canvas : document.createElement('canvas'),
+//  start : function() {
+//    this.canvas.width = 900;
+//    this.canvas.height = 400;
+//    this.canvas.id = 'canvas';
+//    this.context = this.canvas.getContext("2d");
+//    document.getElementById('game-board').appendChild(this.canvas);
+//  }
+//}
+//myGameArea.start();
