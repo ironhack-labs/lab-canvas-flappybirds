2b0c253f0c80bec0072b4291b2fb4f6dc96a0c87
Josue Valencia <josue.valencia@domo.ec>
2019-02-24 10:28:22 +0100

    the background is not moving

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,7 +4,43 @@ window.onload = function() {
   };
 
   function startGame() {
-
+    canvas.start();
   }
 
 };
+
+
+let canvas = {  
+  image : new Image(),  
+  canvas : document.createElement('canvas'), 
+  start : function () {
+    this.context = this.canvas.getContext('2d')
+    this.canvas.width = 880;
+    this.canvas.height = 470;
+    this.image.src = './images/bg.png';   
+    this.image.onload = this.updateCanvas();  
+    this.canvas.style.border = "thick solid #0000FF";
+    document.getElementById('game-board').appendChild(this.canvas);
+  },  
+  bkgMove : function () {
+    this.speed = -1;
+    this.x = 0;
+    this.x += this.speed;
+    this.x %= this.canvas.width;
+  },
+  bkgDraw :  function (){
+    this.context.drawImage(this.image, this.x, 0);
+    if (this.speed < 0){
+      this.context.drawImage(this.image, this.x + this.canvas.width, 0);
+    } else {
+      this.context.drawImage(this.image, this.x - this.image.width, 0);      
+    }
+  },
+  updateCanvas : function (){
+    setInterval(()=>{
+      this.bkgMove()
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.bkgDraw();
+    }, 20)
+  }
+}
