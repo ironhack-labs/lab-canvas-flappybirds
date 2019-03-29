8b0e38e6212b1f4e15dbde4a2d53119e6bba4775
Francisco Vaca <fcovacarh@gmail.com>
2019-03-29 16:50:47 +0100

    Canvas with background now is created when start game is pressed

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,26 @@
+var canvas, canvasW, canvasH, ctx;
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
+    var gameBoard = document.querySelector('#game-board');
+    canvas = document.createElement('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasW = canvas.width;
+    canvasH = canvas.height;
 
+    gameBoard.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+
+    var bgImage = new Image();
+    bgImage.src = './images/bg.png';
+    bgImage.onload = function() {
+      ctx.drawImage(bgImage, 0, 0, canvasW, canvasH);
+    }
   }
 
 };
