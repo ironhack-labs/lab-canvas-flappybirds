196be4aa0e3113758af3bf5f8237807fe4fa38a8
rogerayra <roger.ayra@gmail.com>
2019-06-14 16:05:36 -0500

    Añadido el score

--- helpers.js
+++ helpers.js
@@ -5,31 +5,40 @@ function generatePipes() {
   let randomHeight = Math.floor(Math.random() * 250) + 50;
 
   let pipeTop = new Pipe(0, randomHeight, true);
-  let pipeBottom = new Pipe(randomHeight + ventanita, canvas.height - randomHeight + ventanita);
+  let pipeBottom = new Pipe(
+    randomHeight + ventanita,
+    canvas.height - randomHeight + ventanita
+  );
 
   pipes.push(pipeTop, pipeBottom);
 }
 
-function drawPipes(){
+function drawPipes() {
+  removePipes();
 
-    pipes.filter(pipe)
-
-    pipes.forEach((pipe) => pipe.draw());
+  pipes.forEach(pipe => pipe.draw());
 }
 
-function pipeIsOut(pipe){
-    return (
-        pipe.x + pipe.width < 0
-    );
+function pipeIsOut(pipe) {
+  return pipe.x + pipe.width < 0;
 }
 
-function checkCollition(){
-    let result = false;
-    pipes.forEach(pipe => {
-        if (bird.isTouching(pipe)){
-            result = true;
-        }
-    })
+function removePipes() {
+  for (let i = 0; i < pipes.length; i++) {
+    if (pipeIsOut(pipes[i])) {
+      pipes.splice(i, 1);
+      i--;
+    }
+  }
+}
 
-    return result;
+function checkCollition() {
+  let result = false;
+  pipes.forEach(pipe => {
+    if (bird.isTouching(pipe)) {
+      result = true;
+    }
+  });
+
+  return result;
 }
