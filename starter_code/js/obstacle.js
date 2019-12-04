ec3c6dbb6c499b799f7c49684765cce10efc5fca
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-12-04 03:57:38 -0500

    top obstacle added

--- starter_code/js/obstacle.js
+++ starter_code/js/obstacle.js
@@ -1,18 +1,23 @@
 class Obstacle {
   constructor() {
+    this.ctx = canvas.getContext('2d');
     this.width = 50;
-    this.height = 100;
-    this.x = undefined;
-    this.y = undefined;
+    this.height = 200;
+    this.x = canvas.width;
+    this.y = 0;
+    this.vx = 0.5;
     this.obstacles = [
       { name: 'top', src: './images/obstacle_top.png' },
       { name: 'bottom', src: './images/obstacle_bottom.png' },
     ];
     this.obs = new Image();
   }
-  getObstacle = () => {
-    let index = Math.floor(Math.random() * this.obstacles.length);
-    this.obs.src = this.obstacles[index];
-    ctx.drawImage(this.obs);
+  getTopObstacles = () => {
+    // let index = Math.floor(Math.random() * this.obstacles.length);
+    // this.obs.src = this.obstacles[index];
+    this.x -= this.vx;
+    this.obs.src = this.obstacles[0].src;
+    this.ctx.drawImage(this.obs, this.x, this.y, this.width, this.height);
+    console.log('Output for: Obstacle -> getTopObstacles -> this.x', this.x);
   };
 }
--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -6,15 +6,17 @@ window.onload = function() {
   document.getElementById('game-board').appendChild(canvas);
   canvas.setAttribute('id', 'canvas');
   const ctx = canvas.getContext('2d');
+  canvas.width = 700;
+  canvas.height = 500;
   const player1 = new Player(canvas.width / 2, canvas.height / 2);
+  const obstacle = new Obstacle();
   startGame = () => {
-    canvas.width = 700;
-    canvas.height = 500;
     const bgImage = new this.Image(); //background img
     bgImage.src = './images/bg.png';
     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
     player1.keyEvent();
     player1.loop();
+    obstacle.getTopObstacles();
     // const birdImg = new this.Image();
     // birdImg.src = './images/flappy.png';
     // ctx.drawImage(birdImg, canvas.width / 2, canvas.height / 2, 30, 30);
