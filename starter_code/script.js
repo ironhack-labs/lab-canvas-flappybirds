631996def0b88c03afb1f51ab75016e30a53dcb8
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2019-12-07 14:27:46 +0100

    Update 7 Dic

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,6 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
+  document.getElementById("start-button").onclick = () => {
+    let game = new Game();
+    game.init();
   };
-
-  function startGame() {
-
-  }
-
 };
