b6ba73ceb5b381e1466b2aa3dedced702e8314b6
Esteban <esteb.gtz.mtz@hotmail.com>
2020-01-19 17:50:22 -0600

    DE-WDFT-MEX-Esteban-Gutierrez
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- starter_code/script.js
+++ starter_code/script.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 let frames = 0;
 let interval;
+let obstacles = [];
+let score = 0;
 
 const imgs = {
     background: "./images/bg.png",
@@ -43,26 +45,104 @@ class FlappyBird {
         }
     }
     draw() {
+        this.y += 5;
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
     fly() {
-        this.y -= 25;
+        this.y -= 50;
+    }
+    isTouching(pipe) {
+        return (
+            this.x < pipe.x + pipe.width &&
+            this.x + this.width > pipe.x &&
+            this.y < pipe.y + pipe.height &&
+            this.y + this.height > pipe.y
+        )
+    }
+}
+
+class Pipe {
+    constructor(y, height, imgType) {
+        this.x = canvas.width;
+        this.y = y;
+        this.height = height;
+        this.width = canvas.width / 5;
+        this.imgTop = new Image();
+        this.imgBottom = new Image();
+        this.imgTop.src = imgs.pipeTop;
+        this.imgBottom.src = imgs.pipeBottom;
+        this.imgType = imgType;
+    }
+    draw() {
+        this.x--;
+        if (this.imgType) {
+            ctx.drawImage(this.imgTop, this.x, this.y, this.width, this.height);
+        } else {
+            ctx.drawImage(this.imgBottom, this.x, this.y, this.width, this.height);
+        }
     }
 }
 
 const background = new Background();
 const flappy = new FlappyBird();
 
+const generatePipes = () => {
+    if (frames % 300 === 0) {
+        const min = 100;
+        const max = 300;
+        const spaceBetweenPipes = 500;
+        const randomHeight = Math.floor(Math.random() * (max - min)) + min;
+        obstacles.push(new Pipe(0, randomHeight, false));
+        obstacles.push(
+            new Pipe(randomHeight + spaceBetweenPipes, canvas.height - randomHeight, true)
+        )
+        console.log(obstacles);
+    }
+}
+
+const drawPipe = () => {
+    obstacles.forEach(pipe => pipe.draw());
+}
+
+
+const checkCollitions = () => {
+    if (flappy.y >= canvas.height - flappy.height) return gameOver()
+    obstacles.forEach((pipe, i) => {
+        if (pipe.x + pipe.width <= 0) {
+            obstacles.splice(i, 1)
+        }
+        flappy.isTouching(pipe) ? gameOver() : null
+    })
+}
+
+const drawScore = () => {
+    ctx.font = "20px Arial"
+    ctx.fillStyle = "white"
+    ctx.fillText(`Score: ${score}`, canvas.width - 200, 50);
+}
+const gameOver = () => {
+    const title = "GAME OVER"
+    const msj = `Your score is: ${score}`
+
+    ctx.fillStyle = 'red'
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillText(title, (canvas.width / 2) - 50, 300)
+    ctx.fillText(msj, (canvas.width / 2) - 60, 320)
+    obstacles = []
+    clearInterval(interval)
+}
+
 const update = () => {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     background.draw();
     flappy.draw();
+    if (frames % 50 === 0) { score++ }
+    generatePipes()
+    drawPipe();
+    checkCollitions();
+    drawScore();
 }
-
-functio
-
-
 window.onload = function() {
     document.getElementById("start-button").onclick = function() {
         startGame();
@@ -76,6 +156,7 @@ window.onload = function() {
     })
 
     function startGame() {
+        if (interval) return;
         interval = setInterval(update, 1000 / 60)
     }
 
