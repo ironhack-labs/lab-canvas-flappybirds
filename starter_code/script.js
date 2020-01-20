062dc9eacb6ae9399ea37f24959996dce7d273e6
Carlos Fabián <carl.fab.g@gmail.com>
2020-01-19 20:22:52 -0600

    cuarta iteracion completa

--- starter_code/script.js
+++ starter_code/script.js
@@ -17,7 +17,8 @@ const images = {
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
-    start()
+   
+   
   };
 }
 
@@ -26,7 +27,7 @@ class Board {
     this.x = 0,
     this.y = 0,
     this.width = canvas.width,
-    this.heigth = canvas.height,
+    this.heigth = canvas.height, //this can generate a mistake
     this.img = new Image(),
     this.img.src = images.background
   }
@@ -34,7 +35,7 @@ class Board {
   draw(){
     ctx.drawImage(this.img, this.x, this.y, this.width, this.heigth );
     ctx.drawImage(this.img, this.x + canvas.width, this.y, this.width, this.heigth )
-    this.x -=10
+    this.x -=2
     if(this.x < -canvas.width){
       this.x = 0
     }
@@ -53,14 +54,38 @@ class Bird {
 
   draw(){
     ctx.drawImage(this.birdImg, this.speedX,this.speedY, this.width, this.heigth)
-    this.speedY +=30
+    this.speedY +=2
     
   }
 
   gravity(){
-    this.speedY -= 20
+    this.speedY -=20
   }
 
+}
+
+class Pipes {
+  constructor(y,heigth,imgType){
+    this.x = canvas.width,
+    this.y = y,
+    this.width = canvas.width / 5,
+    this.heigth = heigth,
+    this.pipeBot = new Image(),
+    this.pipeTop = new Image(),
+    this.pipeBot.src = images.obstacleBottom,
+    this.pipeTop.src = images.obstacleTop,
+    this.imgType = imgType
+  }
+
+  draw(){
+    this.x -= 1
+    if(this.imgType){
+      ctx.drawImage(this.pipeTop, this.x, this.y, this.width, this.heigth)
+      
+    }else {
+      ctx.drawImage(this.pipeBot, this.x, this.y, this.width, this.heigth)
+    }
+  }
 }
 
 
@@ -72,14 +97,38 @@ function startGame() {
   bird.draw()
 }
 
+
+function generatePipes(){
+  if(frames % 300 === 0){
+   const min = 100
+   const max = 250
+   const spaceBetween = 100
+   const randomHeight = Math.floor(Math.random() * (max - min)) + min
+   obstacles.push(new Pipes(0,randomHeight,true))
+   obstacles.push(new Pipes(randomHeight + spaceBetween, canvas.height - randomHeight, false))
+  }
+console.log(obstacles)
+}
+
+function drawPipes(){
+  obstacles.forEach( pipe => pipe.draw())
+}
+
+
 function update() {
+frames += 1
+//ctx.clearRect(0, 0, canvas.width, canvas.height)
 startBoard.draw()
 bird.draw()
-bird.gravity()
+//bird.gravity()
+generatePipes()
+drawPipes()
+
+
 }
 
 function start(){
-  interval = setInterval(update,800, 60);
+  interval = setInterval(update, 1000 / 60);
 }
 
 
@@ -88,5 +137,9 @@ document.addEventListener('keydown', ({keyCode}) => {
 
 if(keyCode == 32) {
   bird.gravity();
+  
 }
 })
+
+start()
+
