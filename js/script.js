0f9ec9221be9c37f5c20aaa583d17b2c51364afd
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-20 19:42:38 +0200

    add background canvas

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ window.onload = function() {
   }
 
 };
+
+// The Canvas
+const canvas = document.getElementById("my-canvas");
+let ctx = canvas.getContext("2d");
+
+// The Background Canvas
+const backgroundCanvas = document.getElementById("my-canvas-background");
+let backgroundCtx = backgroundCanvas.getContext("2d");
