fb0aaa2808cfc949dba09da2ee900335fdc02dd9
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-20 19:47:12 +0200

    add class background canvas

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+  document.getElementById("start-button").addEventListener("click", startGame);
 
   function startGame() {
 
   }
-
 };
 
 // The Canvas
@@ -16,3 +13,33 @@ let ctx = canvas.getContext("2d");
 // The Background Canvas
 const backgroundCanvas = document.getElementById("my-canvas-background");
 let backgroundCtx = backgroundCanvas.getContext("2d");
+
+class BackgroundCanvas {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.width = backgroundCanvas.width;
+      this.height = backgroundCanvas.height;
+      this.img = new Image();
+      this.img.src = "../images/bg.png";
+      this.speed = 2;
+  }
+
+  drawRoad() {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+          this.img,
+          this.x,
+          this.y - backgroundCanvas.height,
+          this.width,
+          this.height
+      );
+  }
+
+  move() {
+      this.y += this.speed;
+      this.y %= backgroundCanvas.height;
+  }
+}
+
+let background = new BackgroundCanvas(0, 0);
