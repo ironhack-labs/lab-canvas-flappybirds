d4441d57c1e4ca2a6ed03f31dbc84d26865b6860
Luisgp01 <73586516+Luisgp01@users.noreply.github.com>
2021-12-04 12:26:36 +0100

    First Iteration

---
+++ js/Game.js
@@ -0,0 +1,28 @@
+class Game {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.intervalId = undefined;
+        this.background = new Background(ctx)
+
+    }
+
+    start() {
+        if (!this.intervalId) {
+            this.intervalId = setInterval(() => {
+                this.clear();
+                this.move();
+                this.draw();
+            }, 1000 / 60);
+        }
+    }
+    clear() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+    }
+
+    draw() {
+        this.background.draw();
+    }
+    move() {
+
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+    document.getElementById("start-button").onclick = function() {
+        startGame();
+    };
 
-  function startGame() {
+    const canvas = document.getElementById("my-canvas");
+    const ctx = canvas.getContext("2d")
 
-  }
+    function startGame() {
+        const game = new Game(ctx);
+        game.start()
+
+    }
 
 };
