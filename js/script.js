a488d953b768a0033c03138324b48b39aaba6747
Thais Fernandes <thais.costafernandesf@gmail.com>
2022-03-01 22:32:17 +0000

    script

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
+window.onload = function () {
+  const canvas = document.getElementById("my-canvas");
+  const ctx = canvas.getContext("2d");
+  let game = undefined;
 
   function startGame() {
+  document.addEventListener("keydown", (e) => {
+    game.onKeyUp(e);
+  });
 
   }
+  document.addEventListener("keyup", (e) => {
+    game.onKeyDown(e);
+  });
 
+  document.getElementById("start-button").onclick = function () {
+    game = new Game(ctx);
+    game.startGameLoop();
+  };
 };
