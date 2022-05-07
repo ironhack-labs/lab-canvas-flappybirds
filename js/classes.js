a3031e0874b24fffff69a3f162f379b340fa1f78
ana luz martinez <almadigital.mx@gmail.com>
2022-05-07 12:03:43 -0700

    version 2 aun con error

--- js/classes.js
+++ js/classes.js
@@ -9,11 +9,24 @@ class Background {
         this.width = w;
         this.height = h;
         this.img = new Image() //this gives imgBg lots of methods, like src, onload, etc.
-        this.img.src =  ("images/bg.png")
+        this.img.src =  "images/bg.png"
     }
     //methods
     render(){
-        ctx.drawImage(this.img,this.x,this.y,this.width,this.heigth)
+        //infinite loop
+        if(this.x < -canvas.width){
+            this.x=0
+        }
+        //move canvas
+        this.x--; // x axis
+
+        //drawImage(img,x,y,w,h)
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.heigth);
+        //repeat bg
+        ctx.drawImage(this.img,
+        this.x + this.width,
+        this.y,
+        this.height);
     }
     
 }
@@ -28,7 +41,7 @@ class Flappy{
        this.width = w;
        this.height = h;
        this.img = new Image() //this gives imgBg lots of methods, like src, onload, etc.
-       this.img.src =  ("images/flappy.png")
+       this.img.src =  "images/flappy.png"
        //gravity
        this.vy=2
        //power to raise
@@ -36,6 +49,21 @@ class Flappy{
     }
     //methods
     render(){
+        //validate gravity
+        this.vy = this.vy + (gravity - this.userPull)
+
+        //validate that gflappy doesnt leave y = 0
+        if (this.y <=0){
+            this.userPull = 0;
+            this.y = 2;
+            this.vy=2;
+        }
+
+        //modify gravity by Y
+        if (this.y+this.height < canvas.height){
+            this.y += this.vy;
+        }
+
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
     }
     //collision. returns true || false. item being object or enemy
@@ -53,12 +81,23 @@ class Flappy{
 
 //ENEMY
 
-class Pipe extends Flappy {
-    //pos "position" (top); top, x,y,h
-    contructor(pos,x,y,h){
-        super(x,y,50,h)
-        this.img.src=
-            pos ==="top"//condition
-            ?"images/obstacle_top.png":"images/obstacle_bottom.png"
-    }
+class Pipe extends Flappy{
+    //(top,x,y,h)
+constructor(pos,x,y,h){
+super(x,y,50,h)
+this.img.src=
+pos === "top"// condition
+? "images/obstacle_top.png"
+:"images/obstacle_bottom.png"
+}
+render(){
+this.x -= 2;
+ctx.drawImage(
+    this.img,
+    this.x,
+    this.y,
+    this.width,
+    this.height
+)
+}
 }
--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 window.onload = function() {
   //always declare consts or lets that we'll use
-  const bg = new Background (canvas.width, canvas.height)
-  const flappy = new Flappy (50,40,35,35)
+  const bg = new Background (canvas.width, canvas.height);
+  const flappy = new Flappy (50,40,35,35);
 
   document.getElementById("start-button").onclick = function() {
-    startGame();
+    if(!requestId){
+      startGame();
+    }
+   
   };
 
   function startGame() {
       //start gamr v 0.1
-      updateGame()
-  }
+      //updateGame()
+      //ver v 0.2
+      requestId= requestAnimationFrame(updateGame)
+    }
 
   //how we loose
   function gameOver(){
+    console.log("te moriste");
+    requestId = undefined;
+  }
+
+  function resetGame(){
 
   }
   //how we win
@@ -27,15 +37,69 @@ window.onload = function() {
   function updateGame(){
     //clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    
     bg.render()
+
     flappy.render()
+    
+    rePipes()
+    generatePipes()
+    
+    if(flappy.y + flappy.height > canvas.height){
+      gameOver()
+    }
+
+    if (requestId){ //recursividad
+      requestAnimationFrame(updateGame)
+    }
 
     requestAnimationFrame(updateGame)
   }
 
   //generate and render pipes
   function generatePipes(){
- 
+    //limit amount of pipes
+    if (!(frames %160 ===0)){
+      return true //do nothing until you meet this condition
+    }
+
+    //heigth random
+    //math.floor( math.random()*(max * 0.6)) + 35 (0.6 minimum value, 35 is our flappy size)
+    const height = math.floor(math.random()*(canvas.height * 0.6)) + 35;
+    const pipe1= new Pipe("top", canvas.width,0,heigth) //so it comes from the right side
+    const pipe2= new Pipe ("bottom", canvas.width, height + 120, canvas.height - 120 - height) //pos, x, y, dynamic height
+    pipes.push(pipe1,pipe2)
+
   }
-  function renderPipes(){}
-};
+  function renderPipes(){
+    //forEach => doesnt return anything
+    //for of => doesnt return
+    // for => doesnt return
+    //map => returns a new array, copy of the original
+    //reduce => returns an array, object, number, string
+    //filter => returns a new filtered array
+    // we will use For Each becauuse we only need to iterate
+
+    pipes.forEach(pipe,index_pipe)=>{
+      //render pipes
+      pipe.render()
+    }
+  }
+
+  addEventListener("keydown",(event)=>{
+    event.preventDefault();
+
+    if (event.keyCode ===32){
+      //this.y -= 10; //v1 will be deleted
+      flappy.userPull = 0.3; //v2 
+    }
+    //32 es espacio
+    })
+    addEventListener("keyup", (event)=>{
+      event.preventDefault()
+
+      if(event.keyCode === 32) {
+        flappy.userPull = 0;
+      }
+    })
+  };//end onload
