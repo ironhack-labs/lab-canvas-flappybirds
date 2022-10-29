e39b07896a1e440634bd5c9cbc4cdcc7ccfb707c
David Hatch <hatchdavidv@gmail.com>
2022-10-29 12:50:41 -0500

    bg y flappy

--- js/classes.js
+++ js/classes.js
@@ -13,14 +13,48 @@ class Background{
         this.height = h;  
         // Image
         this.image = new Image(); 
-        this.image.src = '../images/bg.png' 
+        this.image.src = 'images/bg.png' 
     }
 
     update(){
-        
+        if(this.x  < -canvas.width){
+            this.x =0; 
+        }
+        // Generar el movimiento de la imagen 
+        this.x--;
         // ctx = context 
         //Éste método recibe 5 parámetros .drawImage(img, posición x, posición y, this.width)
         ctx.drawImage(this.image, this.x, this.y , this.width, this.height)
-
+        // Re dibujar la imagen
+        ctx.drawImage(
+            this.image, 
+            this.x + this.width, // Agregar la imagen 2 al final de la imagen 1
+            this.y, 
+            this.width, 
+            this.height
+        )
     }
+}
+
+class Flappy{
+    /**
+     * @param {number} x => 
+     * @param {number} y 
+     * @param {number} w 
+     * @param {number} h 
+     */
+    constructor(x,y,w,h){   
+        this.x = x;
+        this.y = y; 
+        this.width = w; 
+        this.height = h; 
+        // Image 
+        this.image = new Image();
+        this.image.src = 'images/flappy.png'    
+        
+        this.vy = 2 //
+        this.userPull = 0 // Movimiento
+    }
+    update(){}
+    collision(){}
 }
--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 // Window = {...methods, properties}
 window.onload = function() {
+  const bg = new Background(canvas.width , canvas.height )
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-
+    // Ejecutar update game
+    
+    updateGame()
   }
 
+  function updateGame(){
+    // .clearRect (x,y,width, height)
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    bg.update()
+    requestAnimationFrame(updateGame)
+    
+  }
 };
+
+
